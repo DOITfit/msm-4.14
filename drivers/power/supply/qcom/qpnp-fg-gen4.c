7e56ef292fdc758b54f61dbe64523b9180630ecc
Julian Liu <wlootlxt123@gmail.com>
2020-03-07 18:08:32 +0800

    treewide: Fix clang warnings
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -6302,7 +6302,6 @@ static int fg_gen4_resume(struct device *dev)
 {
 	struct fg_gen4_chip *chip = dev_get_drvdata(dev);
 	struct fg_dev *fg = &chip->fg;
-	int val = 0;
 
 	if (!fg->input_present)
 		fg_get_batt_isense(fg, &val);
