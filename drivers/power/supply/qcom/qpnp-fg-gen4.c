f398862e4b170acbdc6328f5a6b2ce763e4edcfe
DOITfit <w175156@outlook.com>
2020-07-16 15:18:30 +0800

    Revert treewide: Fix clang warnings

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -6270,6 +6270,7 @@ static int fg_gen4_resume(struct device *dev)
 {
 	struct fg_gen4_chip *chip = dev_get_drvdata(dev);
 	struct fg_dev *fg = &chip->fg;
+    int val = 0;
 
 	if (!fg->input_present)
 		fg_get_batt_isense(fg, &val);
